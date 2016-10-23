c9df8b83058a4ffc138b38e2cc9a8c18526d12e4
Shu <qiangdaxs888>
2016-10-23 01:23:07 -0400

    Introduce HTML to HelloDjango

--- hello/views.py
+++ hello/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse(u'qiangdaxs')
+#def index(request):
+    #return HttpResponse(u'qiangdaxs')
+
+def post_list(request):
+    return render(request, 'hello/post_list.html', {})
